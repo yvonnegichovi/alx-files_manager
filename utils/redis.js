408cee96d44d1f21a655751a3def0140e2a9aba0
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-07 18:06:13 +0300

    files manager

--- utils/redis.js
+++ utils/redis.js
@@ -1,13 +1,36 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('error', (err) => {
-      console.error('Redis client error:', err);
-    });
-    this.client.connect().catch((err) => {
-      console.error('Redis client connection error:' err);
+    this.redisClient = createClient();
+    this.redisClient.on('error', (error) => {
+      console.log(error.message);
     });
   }
+  isAlive() {
+    return this.redisClient.connected;
+  }
+  async get(key) {
+    const asyncGet = promisify(this.redisClient.get).bind(this.redisClient);
+    const value = await asyncGet(key);
+    return value;
+  }
 
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.redisClient.set).bind(this.redisClient);
+    await asyncSet(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const asyncDel = promisify(this.redisClient.del).bind(this.redisClient);
+    await asyncDel(key);
+  }
+
+  async close() {
+    this.redisClient.quit();
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
