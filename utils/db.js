c629962ad57502d0c3d64a567acf4080f569268f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-07 19:38:26 +0300

    files manager

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,39 @@ class DBClient {
   constructor() {
     const HOST = process.env.DB_HOST || 'localhost';
     const PORT = process.env.DB_PORT || '27017';
-    const DB = process.env.DB_DATABASE || 'files_manager';
+    const DATABASE = process.env.DB_DATABASE || 'files_manager';
     const URI = `mongodb://${HOST}:${PORT}`;
-    thi.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
+    this.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
     this.mongoClient.connect((error) => {
       if (!error) this.db = this.mongoClient.db(DATABASE);
     });
   }
+
+  isAlive() {
+    return this.mongoClient.isConnected();
+  }
+
+  getCollection(collectionName) {
+    const collection = this.db.collection(collectionName);
+    return collection;
+  }
+
+  async nbUsers() {
+    const usersCollection = this.getCollection('users');
+    const numberOfUsers = await usersCollection.countDocuments();
+    return numberOfUsers;
+  }
+
+  async nbFiles() {
+    const filesCollection = this.getCollection('files');
+    const numberOfFiles = filesCollection.countDocuments();
+    return numberOfFiles;
+  }
+
+  async close() {
+    await this.mongoClient.close();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
